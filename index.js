112d794486e1eed22a4074770fd13b1e15adcb50
maksrover <maksrover@gmail.com>
2023-07-27 16:30:40 +0300

    zadanie5

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let a = 10;
-alert (a);
+let alfa = 10;
+alert(alfa);
 
-let b = 2007;
-alert (b);
+let beta = 2007;
+alert(beta);
 
-let c = Брендан Эйх или Айк;
-alert (c);
+let cto = 'Брендан Эйх';
+alert(cto);
+
+let d = 10;
+let e = 2;
+let resultA = d + e;
+let resultB = d - e;
+let resultC = d * e;
+let resultD = d / e;
+
+alert(`${resultA} ${resultB} ${resultC} ${resultD}`);
+
+let small = 2;
+let over = 5;
+
+let result = small ** 5;
+
+alert(result);
+
+
