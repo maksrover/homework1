dba5e79a349a56cbfb8eb682812ad988ef0b5997
maksrover <maksrover@gmail.com>
2023-07-27 16:32:35 +0300

    zadanie6

--- index.js
+++ index.js
@@ -13,17 +13,14 @@ let resultA = d + e;
 let resultB = d - e;
 let resultC = d * e;
 let resultD = d / e;
-
 alert(`${resultA} ${resultB} ${resultC} ${resultD}`);
 
 let small = 2;
 let over = 5;
-
 let result = small ** 5;
-
 alert(result);
 
 let a = 9;
 let b = 2;
-alert(a/b);
+alert(a / b);
 
