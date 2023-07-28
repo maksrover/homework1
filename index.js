95221c0125b402fa3e38e2f5e0f0488f0c4c87dc
maksrover <maksrover@gmail.com>
2023-07-28 22:26:00 +0300

    7 zadanie ispravil

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let alfa = 10;
-alert(alfa);
+alert(alfa); // zadanie1
 
 let beta = 2007;
-alert(beta);
+alert(beta); // zadanie2
 
 let cto = 'Брендан Эйх';
-alert(cto);
+alert(cto); // zadanie3
 
 let d = 10;
 let e = 2;
@@ -13,25 +13,25 @@ let resultA = d + e;
 let resultB = d - e;
 let resultC = d * e;
 let resultD = d / e;
-alert(`${resultA} ${resultB} ${resultC} ${resultD}`);
+alert(`${resultA} ${resultB} ${resultC} ${resultD}`); // zadanie4
 
 let small = 2;
 let over = 5;
 let result = small ** 5;
-alert(result);
+alert(result); //zadanie5
 
 let a = 9;
 let b = 2;
-alert(a / b);
+alert(a / b); // zadanie6
 
 let num = 1;
-num = num + 5;
-num = num - 3;
-num = num * 7;
-num = num / 3;
-num = num++;
-num = num--;
-alert(num);
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num); // zadanie7
 
 let age = prompt("Сколько вам лет?");
 alert(age);
