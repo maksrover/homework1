bc3d9f65641db09146ada916b2619aefcfff2c61
maksrover <maksrover@gmail.com>
2023-08-04 13:34:47 +0300

    zadanie2

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -1,8 +1,12 @@
 
-let count = 0;
+let salut = 0;
 
-while (count < 2) {
+while (salut < 2) {
   console.log("Привет");
-  count++;
-}
+  salut++;
+} //Выведите в консоль 2 раза слово «Привет»
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  } // Выведите в консоль цифры от 1 до 5.    
 
