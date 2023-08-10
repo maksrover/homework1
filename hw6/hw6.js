9911a38f23b173185502f5e54dad5af3a7c8ee2c
maksrover <maksrover@gmail.com>
2023-08-10 17:23:14 +0300

    zadanie3

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -13,3 +13,7 @@ const index = zadanie2.indexOf(4);
 
 console.log(` ${index}`); // Найдите позицию (индекс) числа 4 в этом массиве.
 
+const zadanie3 = [1, 3, 5, 10, 20];
+const result = zadanie3.join(' ');
+
+console.log(result); // С помощью метода join выведите элементы массива через пробел (пустую строку ' ').
