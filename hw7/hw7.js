1f701242585abe43c3d39c1eed5aff9555d4cfe5
maksrover <maksrover@gmail.com>
2023-08-15 20:43:11 +0300

    zadanie5

--- hw7/hw7.js
+++ hw7/hw7.js
@@ -8,12 +8,12 @@ function zadanie2(arr, searchString) {
   const result = [];
 
   for (let i = 0; i < arr.length; i++) {
-      const currentString = arr[i].toLowerCase();
-      const searchSubstring = searchString.toLowerCase();
+    const currentString = arr[i].toLowerCase();
+    const searchSubstring = searchString.toLowerCase();
 
-      if (currentString.startsWith(searchSubstring)) {
-          result.push(arr[i]);
-      }
+    if (currentString.startsWith(searchSubstring)) {
+      result.push(arr[i]);
+    }
   }
 
   return result;
@@ -21,22 +21,39 @@ function zadanie2(arr, searchString) {
 
 const array1 = ['Кошка', 'Кит', 'Комар', 'Носорог'];
 const searchString1 = 'ко';
-console.log(zadanie2(array1, searchString1)); 
+console.log(zadanie2(array1, searchString1));
 
 const array2 = ['яблоко', 'груша', 'гриб', 'огурец'];
 const searchString2 = 'гру';
-console.log(zadanie2(array2, searchString2)); 
+console.log(zadanie2(array2, searchString2));
 
 const array3 = ['Дом', 'Банк', 'Больница', 'Театр'];
-const searchString3 = 'Кино'; 
+const searchString3 = 'Кино';
 console.log(zadanie2(array3, searchString3)); // [] // zadanie2 Создайте функцию, которая в качестве параметров принимает массив строк и строку.
 
-let number = 32.58884;
+let zadanie3 = 32.58884;
 
-let roundedMin = Math.floor(number); 
-let roundedMax = Math.ceil(number);
-let roundedNearest = Math.round(number); 
+let roundedMin = Math.floor(zadanie3);
+let roundedMax = Math.ceil(zadanie3);
+let roundedNearest = Math.round(zadanie3);
 
-console.log( roundedMin); 
-console.log( roundedMax); 
-console.log( roundedNearest); // zadanie3 Округлите число 32.58884 
+console.log(roundedMin);
+console.log(roundedMax);
+console.log(roundedNearest); // zadanie3 Округлите число 32.58884 
+
+let zadanie4 = [52, 53, 49, 77, 21, 32];
+
+let minNumber = Math.min(...zadanie4);
+let maxNumber = Math.max(...zadanie4);
+
+console.log(minNumber);
+console.log(maxNumber); // zadanie4 Даны числа 52, 53, 49, 77, 21, 32. Необходимо найти среди этих чисел наименьшее и наибольшее числа и вывести их в консоль.
+
+
+function getRandomNumber() {
+  const zadanie5 = Math.floor(Math.random() * 10) + 1;
+  console.log(zadanie5);
+}
+
+getRandomNumber(); // Создайте функцию, которая будет выводить в консоль рандомное число от 1 до 10.
+
